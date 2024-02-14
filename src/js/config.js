a5cc503ff0caf53272dd969756d05d9250afc38b
Boris Abramov <borisab93@gmail.com>
2024-02-14 12:02:41 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f7ff0c00-f65b-4253-8335-8aac2b4e8a5f';
+export const MODAL_CLOSE_SEC = 2.5;
